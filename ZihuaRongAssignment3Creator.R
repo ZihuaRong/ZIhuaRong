cf71c2351ae3e6aa41fe7b5ef6946635163a0c87
ZihuaRong <zirong@ucsc.edu>
2016-02-04 23:53:15 -0800

    ZihuaRongAssignment3

---
+++ ZihuaRongAssignment3Creator.R
@@ -0,0 +1,67 @@
+# n #Assignment3##
+##Question0
+firstName<- "Zihua"
+lastName <- "Rong"
+print(paste(firstName,lastName))
+
+studentID<- "1505002"
+print(studentID)
+
+email <- "zirong@ucsc.edu"
+print(email)
+
+##Question1
+
+#install package pdlyr
+install.packages("dplyr")
+library(dplyr)
+#install library foreign
+library(foreign)
+df.ex <- read.dta("https://github.com/EconomiCurtis/econ294_2015/raw/master/data/org_example.dta")
+class(df.ex)
+
+##Question2
+df.ex.2a <- dplyr::filter(df.ex,year == 2013 & month == 12)
+print(nrow(df.ex.2a))
+df.ex.2b <- dplyr::filter(df.ex,year == 2013 & (month == 7 | month == 8 | month == 9))
+print(nrow(df.ex.2b))
+
+##Question3 
+df.ex.3a <- dplyr::arrange(df.ex,year,month)
+
+##Question4
+df.ex.4a <- dplyr::select(df.ex,year,age)
+df.ex.4b <- dplyr::select(df.ex,year,month,starts_with("i"))
+print(unique(df.ex$state))
+
+##Question5
+stndz <- function(x){(x - mean(x, na.rm = T))  /  sd(x, na.rm = T)}
+nrmlz <- function(x){(x-min(x, na.rm = T))/(max(x, na.rm = T)-min(x, na.rm = T))}
+
+df.ex.5a<- df.ex %>% 
+mutate(rw.stndz= stndz(rw),rw.nrmlz=nrmlz(rw))
+
+df.ex.5b <- df.ex %>% group_by(year, month) %>%
+  mutate(rw_stndz=stndz(rw), rw_nrmlz=nrmlz(rw), count=n())
+
+##QUestion 6 
+df.ex.6<-
+  df.ex %>% 
+  group_by(year,month,state)%>%
+  summarise(
+    rw_min=min(rw,na.rm = T),
+    rw_1stQnt=quantile(rw,na.rm = T,0.25),
+    rw_mean.art =mean(rw,na.rm = T),
+    rw_3rdQnt=quantile(rw,na.rm = T,0.75),
+    rw_max=max(rw,na.rm = T),
+    rw_median=median(rw,na.rm=T),
+    count=n()
+  )%>%
+  select(state,starts_with("rw_"),count)
+
+print(df.ex.6 %>% ungroup() %>% arrange(desc(rw_mean.art)) %>%
+        select(year,month,state) %>% head(1))
+
+##Question7
+df.ex$state.char <-as.character(df.ex$state)
+df.ex.7a <- df.ex %>% arrange(year,month,desc(state.char))
